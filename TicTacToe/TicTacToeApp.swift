a46a330cf1783ceb12419f4f26ee25fb82e54fc4
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2023-02-06 19:31:01 +0900

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by 14inchの史苑 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
